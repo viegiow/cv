62734735f0a2c48f3d37f1b1145aed3d478233bf
Giovanna Barros <giovanna.viecelli.2000@gmail.com>
2024-02-02 10:29:33 -0300

    changed page structure

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -5,10 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about-me.component.html'
 })
 export class AboutMeComponent implements OnInit {
+idade!: number
+nasc: Date = new Date("2000-07-31")
 
   constructor() { }
 
   ngOnInit(): void {
+    const today = new Date()
+    this.idade = Math.floor((today.getTime() - this.nasc.getTime()) / (1000 * 60 * 60 * 24 * 365))
   }
 
 }
